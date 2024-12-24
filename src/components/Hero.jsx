ede56270dff56cb5193980c32b9d3532820fa658
SaimRazaAly <saimrazateach2010@gmail.com>
2024-12-24 15:07:52 -0800

    Created Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,20 @@
 import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
 import Section from "./Section";
+import {
+  BackgroundCircles,
+  BottomLine,
+  Gradient,
+} from "../components/design/Hero";
+import { heroIcons } from "../Constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
+import Generating from "./design/Generating";
+import Notifications from "./Notifications";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -47,8 +59,29 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => {
+                      return (
+                        <li className="p-5" key={index}>
+                          <img src={icon} width={24} alt={icon} />
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollParallax>
+
+                <ScrollParallax isAbsolutelyPositioned>
+                  <Notifications className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                  title="Code Generation"
+                  />
+                </ScrollParallax>
               </div>
             </div>
+
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
@@ -59,8 +92,13 @@ const Hero = () => {
               alt="hero"
             />
           </div>
+          <BackgroundCircles />
         </div>
+
+<CompanyLogos/>
+
       </div>
+      <BottomLine />
     </Section>
   );
 };
